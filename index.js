298b37febcd8d3b0d35ee0d8d7b4879a1d73519a
Marco <marcocolli293@gmail.com>
2024-10-09 18:38:39 +0200

    strange calculator

---
+++ index.js
@@ -0,0 +1,26 @@
+const display = document.getElementById("display");
+
+function show_on_display(input){
+    display.value += input;
+}
+
+function clearDisplay(){
+    display.value = "";
+}
+
+// strange funtion to calculate
+function calculate(){
+
+    const expression = display.value;
+
+    try{
+        if(expression == "2*3") {
+            display.value = "5";
+        } else {
+            display.value = eval(expression);
+        }
+    }
+    catch(error){
+        display.value = "Error";
+    }
+}
