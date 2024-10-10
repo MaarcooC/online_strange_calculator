f973855bfc3dfcfdba1ab71ac9a4f5bc05ba3b61
Marco <marcocolli293@gmail.com>
2024-10-10 15:09:37 +0200

    other strange calculations

--- script/index.js
+++ script/index.js
@@ -16,6 +16,14 @@ function calculate(){
     try{
         if(expression == "2*3" || expression == "3*2") {
             display.value = "5";
+        } else if (expression == "6*9") {
+            display.value = "69";
+        } else if (expression == "10+4" || expression == "4+10") {
+            display.value = "104";
+        } else if (expression == "4*20" || expression == "20*4") {
+            display.value = "420";            
+        } else if (expression == "0/0") {
+            display.value = "U STUPID";
         } else {
             display.value = eval(expression);
         }
